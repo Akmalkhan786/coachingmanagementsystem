82478b417b82fee6ff0da48e70452bade6101388
Akmal Khan <khan.akmal542@gmail.com>
2020-08-30 02:40:45 +0500

    New Setup Done

--- public/js/app.js
+++ public/js/app.js
@@ -49888,8 +49888,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! I:\laragon\www\CoachingManagementSystem\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! I:\laragon\www\CoachingManagementSystem\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\coachingmanagementsystem\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\coachingmanagementsystem\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
